9a89e9c2e35f4fa0b37605e94fe135240fdd94e3
diego <digomic@outlook.es>
2022-04-24 20:30:32 -0500

    add blog folder

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from unicodedata import name
+from django import views
+from django.urls import path
+from .views import render_posts, post_detail
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', render_posts, name='posts'),
+    path('<int:post_id>', post_detail, name="post_detail")
+]
